d297a6fe93cb15d7f5bd0b033cbd0867f2a3fcc5
Bruno Terceti <bruno.terceti@gmail.com>
2023-11-30 15:15:13 -0300

    new menu + game changes

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const personagem = document.querySelector(".personagem");
 const charizard = document.querySelector(".charizard");
+const pig = document.querySelector(".pig");
 const jogoInfo = document.querySelector(".jogo");
 var telaFinalizar = document.getElementById("telaFinalizar");
 let pontuacao = 0;
 let gameOver = false;
-let dVelocidade = 1.7;
-
-const reiniciarJogoButton = document.getElementById("reiniciarJogo");
+let pigVisivel = false; 
+const intervaloAparecer = 2400; 
+const intervaloDesaparecer = 5200; 
 
 function updatePontuacao() {
   pontuacao++;
   jogoInfo.innerHTML = `Pontuação: <span id="pontuacao" style="color: gold;">${pontuacao}</span>`;
 }
 
-function personagemPassouPeloCharizard() {
+function personagemPassouPeloInimigo() {
   if (!gameOver) {
     updatePontuacao();
   }
@@ -23,13 +24,13 @@ function telaFinalizar_Toogle() {
   if (telaFinalizar.style.display === "none") {
     telaFinalizar.style.display = "block";
     jogoInfo.classList.add("game-over");
-    document.getElementById("pontuacaoFinal").innerText = pontuacao; 
+    document.getElementById("pontuacaoFinal").innerText = pontuacao;
     document.getElementById("pontuacaoFinal").insertAdjacentHTML('afterend', '<span class="pontos-text"></span>');
-	console.log("telaFinalizar.style.display = block");
+    console.log("telaFinalizar.style.display = block");
   } else {
     telaFinalizar.style.display = "none";
-	console.log("telaFinalizar.style.display = none");
-  }		
+    console.log("telaFinalizar.style.display = none");
+  }
 }
 
 const jump = () => {
@@ -42,31 +43,58 @@ const jump = () => {
   }
 };
 
+function desaparecerImagem() {
+  pig.style.display = 'none';
+  setTimeout(aparecerImagem, intervaloAparecer);
+}
+
+function aparecerImagem() {
+  pig.style.display = 'block';
+  pigVisivel = true; 
+  setTimeout(() => {
+    pigVisivel = false; 
+    desaparecerImagem();
+  }, intervaloDesaparecer);
+}
+
+function loopPig() {
+  if (!gameOver) {
+    aparecerImagem();
+  }
+}
+
 const loop = setInterval(() => {
   const charizardPosition = charizard.offsetLeft;
   const personagemPosition = parseInt(window.getComputedStyle(personagem).bottom.replace("px", ""));
+  const pigPosition = pig.offsetLeft; 
 
-  personagemPassouPeloCharizard();
+  personagemPassouPeloInimigo();
 
   if (!charizardPosition) return;
 
-  if (charizardPosition <= 45 && charizardPosition >= -85 && personagemPosition <= 0 && !gameOver) {
+  if (
+    (charizardPosition <= 45 && charizardPosition >= -85 && personagemPosition <= 0) ||
+    (pigVisivel && pigPosition <= 45 && pigPosition >= -85 && personagemPosition <= 0) 
+  ) {
     gameOver = true;
 
     telaFinalizar_Toogle();
 
     charizard.style.animation = "none";
     personagem.style.animation = "none";
+    pig.style.animation = "none";
 
     charizard.style.left = `${charizardPosition}px`;
     personagem.style.bottom = `${personagemPosition}px`;
+    pig.style.left = `${pigPosition}px`;
 
     telaFinalizar_Toogle();
-    
   }
 }, 100);
 
-//telaFinalizar_Toogle();
+loopPig();
+
+const reiniciarJogoButton = document.getElementById("reiniciarJogo");
 
 reiniciarJogoButton.addEventListener("click", () => {
   reiniciarJogo();
